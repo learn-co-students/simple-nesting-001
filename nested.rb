fb8e6fd8e975788f3c1a1ce942b40eb4958b8712
github username <github email address>
2015-11-17 12:51:46 -0500

    done

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -65,7 +67,11 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
+ 		{ 
+      :yukihiro_matsumoto => {
+        :known_for=>"Ruby",
+        :languages=> ["LISP, C"]
+    },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -100,9 +106,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for]="GUI"
+     programmer_hash 
 end
 
 def adding_to_dennis
@@ -121,8 +126,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
-     
+programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+programmer_hash   
 end
 
 
