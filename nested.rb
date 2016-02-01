b78c9e132f0a7b184d0a143da482fef11ebadb22
jprosario <jomi0388@gmail.com>
2016-01-31 21:58:05 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
         }
      }
 
+    programmer_hash[:grace_hopper]
+
 
 end
 
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -80,6 +86,12 @@ def adding_matz
         }
      }
 
+     programmer_hash.merge!(
+      :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+       :languages => ["LISP, C"]
+     })
+
     
 end
 
@@ -101,6 +113,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
      
 end
@@ -121,6 +135,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
+
+     programmer_hash
 
      
 end
